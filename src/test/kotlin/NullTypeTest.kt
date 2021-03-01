b00b461bea1b1d70d31a0354e8abaee186eb9230
Knext <sean.gold@kakaocorp.com>
2021-03-01 19:29:12 +0900

    null check operation

--- src/test/kotlin/NullTypeTest.kt
+++ src/test/kotlin/NullTypeTest.kt
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 
 internal class NullTypeTest {
@@ -50,5 +51,72 @@ internal class NullTypeTest {
         fun strLenSafe(s:String?) :Int = s?.length ?: 0
         assertEquals(3, strLenSafe("abc"))
         assertEquals(0, strLenSafe(null))
+    }
+
+    @Test
+    fun test_elvis2() {
+        class Address(val streetAddress:String, val zipCode:Int, val city:String, val country:String)
+        class Company(val name:String, val address:Address?)
+        class Person(val name:String, val company:Company?)
+        fun printShippingLabel(person:Person) {
+            val address = person.company?.address ?: throw IllegalArgumentException("No Address")
+            with(address) {
+                println(streetAddress)
+                println("$zipCode $city $country")
+            }
+        }
+
+        val address = Address("Ara", 1234, "Seoul", "Korea")
+        val company = Company("JJ", address)
+        val person = Person("Someone", company)
+        printShippingLabel(person)
+        assertThrows(IllegalArgumentException::class.java) {
+            printShippingLabel(Person("Exception", null))
+        }
+    }
+
+    @Test
+    fun test_let() {
+        fun sendEmailTo(email:String) {
+            println("Sendind Email to $email")
+        }
+
+        var email:String? = "aaa@abc.com"
+        email?.let{sendEmailTo(it)}
+
+        email = null
+        email?.let{sendEmailTo(it)}
+    }
+
+    @Test
+    fun test_initialize() {
+        class Service(){
+            fun actionValue(): String = "foo"
+        }
+        class MyService() {
+            fun action():String {
+                service = Service() /*초기화를 수행하지 않으면 lateinit property exception 발생*/
+                return service.actionValue()
+            }
+            private lateinit var service: Service
+        }
+
+        val myService = MyService()
+        assertEquals("foo" , myService.action())
+    }
+
+    @Test
+    fun test_isNullOrBlank() {
+        fun verify(input:String?):String {
+            //return if (input.isNullOrBlank()) "input is blank" else input
+            if (input.isNullOrBlank()) {
+                return "input is blank"
+            }
+            return input
+        }
+
+        assertEquals("input is blank", verify(null))
+        assertEquals("input is blank", verify(" "))
+        assertEquals("test input", verify("test input"))
     }
 }
